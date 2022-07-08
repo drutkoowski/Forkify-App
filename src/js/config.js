359ab6163c1aa29c5860fc3f0bac828b8cfc304b
drutkoowski <d.rutkowski2000@gmail.com>
2022-07-08 13:27:43 +0200

    adding bookmarks and uploading new recipe functionality

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7041dc1e-b9d5-43ae-8431-2401044775bd';
+export const MODAL_CLOSE_SEC = 2.5;
